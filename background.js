e21e5a6ec1b1c7398f10205f1cba9ce8821b4d69
a person <72956230+apersongithub@users.noreply.github.com>
2023-05-03 15:45:47 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//opens youtube on install
 chrome.runtime.onInstalled.addListener(function(details) {
   if(details.reason == "install") {
     chrome.tabs.create({url: "https://www.youtube.com/"});
