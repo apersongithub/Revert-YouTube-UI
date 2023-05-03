2008bbe2e6954db24fe44a0e90ea5c2d51280ab7
a person <72956230+apersongithub@users.noreply.github.com>
2023-05-03 15:47:39 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
-// content.js
-
+// creates set cookie function for easy replacement
 function setCookie(name, value, days, domain, path) {
   let expires = "";
   let secureFlag = "";
@@ -23,16 +22,18 @@ function setCookie(name, value, days, domain, path) {
   document.cookie = cookieString + secureFlag;
 }
 
-// Set the PREF cookie
+// Set the PREF cookie reponsible for your theme
 setCookie("PREF", "f6=8", 365, ".youtube.com", "/");
 
+//the function is self explanatory but the new update doesnt force open it in a new tab anymore
 function openYoutubeVideoInTab(e) {
   if (e.button > 1 || e.altKey) return;
   
   var link = e.target.closest('[href^="/watch"]');
   if (!link || (link.getAttribute('href') || '').match(/^(javascript|#|$)/) || link.href.replace(/#.*/, '') == location.href.replace(/#.*/, '')) return;
-  
+ 
   window.open(link.href, '_self');
+  //cancels the event that youtube uses for dynamic websites
   e.preventDefault();
 }
 
