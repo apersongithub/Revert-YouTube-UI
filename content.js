b01a1872c6c18e4aed7bd65dfd3739dd855e83d9
Jordan Stremming <jcstremming@gmail.com>
2023-05-08 13:39:24 -0500

    code streamlined and commented

--- content.js
+++ content.js
@@ -1,41 +1,34 @@
-// creates set cookie function for easy replacement
-function setCookie(name, value, days, domain, path) {
-  let expires = "";
-  let secureFlag = "";
-  if (days) {
-    let date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  let cookieString = name + "=" + value + expires;
-  if (domain) {
-    cookieString += "; domain=" + domain;
-  }
-  if (path) {
-    cookieString += "; path=" + path;
-  } else {
-    cookieString += "; path=/";
-  }
-  if (window.location.protocol === "https:") {
-    secureFlag = "; secure";
-  }
-  document.cookie = cookieString + secureFlag;
-}
+// helper function to set the document's cookie
+const setCookie = (name, value, days, domain = ".youtube.com", path = "/") => {
+  // if days exists, convert days to seconds for expiration
+  const expires = days ? `max-age=${days * 86400}` : "";
+  //
+  const isSecure = window.location.protocol === "https:" ? "secure" : "";
+  document.cookie = `${name}=${encodeURIComponent(value)}; ${expires}; domain=${domain}; path=${path}; ${isSecure}`;
+};
 
-// Set the PREF cookie reponsible for your theme
-setCookie("PREF", "f6=8", 365, ".youtube.com", "/");
-
-//the function is self explanatory but the new update doesnt force open it in a new tab anymore
-function openYoutubeVideoInTab(e) {
+const openYoutubeVideoInTab = (e) => {
+  // ignore right-click or if Alt key is pressed
   if (e.button > 1 || e.altKey) return;
-  if (e.target.classList.contains('ytp-scrubber-pull-indicator')) return;
-  
-  var link = e.target.closest('[href^="/watch"]');
-  if (!link || (link.getAttribute('href') || '').match(/^(javascript|#|$)/) || link.href.replace(/#.*/, '') == location.href.replace(/#.*/, '')) return;
- 
-  window.open(link.href, '_self');
-  //cancels the event that youtube uses for dynamic websites
+  // ignore if the target is the player's scrubber
+  if (e.target.classList.contains("ytp-scrubber-pull-indicator")) return;
+  // find the closest link element that starts with "/watch"
+  const link = e.target.closest('[href^="/watch"]');
+  // ignore if the link is not valid or a JavaScript/anchor link
+  if (
+    !link ||
+    (link.getAttribute("href") || "").match(/^(javascript|#|$)/) ||
+    link.href.replace(/#.*/, "") === location.href.replace(/#.*/, "")
+  )
+    return;
+  // open the link in the same tab
+  window.open(link.href, "_self");
+  // cancel events YouTube uses for dynamic websites
   e.preventDefault();
-}
+};
 
+// set the PREF value to disable new UI
+setCookie("PREF", "f6=8", 365);
+
+// add listener to open the video with new UI
 window.addEventListener('mouseup', openYoutubeVideoInTab, true);
