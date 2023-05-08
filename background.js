f34ba75862530d93e02798b0d2a6acfc15a1acaf
Jordan Stremming <jcstremming@gmail.com>
2023-05-08 13:39:53 -0500

    use Chrome's defined variable instead of "magic string" for INSTALL

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-//opens youtube on install
-chrome.runtime.onInstalled.addListener(function(details) {
-  if(details.reason == "install") {
+// open YouTube on install
+chrome.runtime.onInstalled.addListener(details => {
+  if(details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.tabs.create({url: "https://www.youtube.com/"});
   }
 });
